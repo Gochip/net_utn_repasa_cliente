17e27f0666d7e3824708aa6333a1358e136e58ba
Gochi <germannparisi@gmail.com>
2014-10-05 17:15:50 -0300

    Agrego funcionalidad de GamesRequestAction y SearchUsersRequestAction

--- src/org/utnrepasa/net/request/SearchUsersRequestAction.java
+++ src/org/utnrepasa/net/request/SearchUsersRequestAction.java
@@ -4,21 +4,36 @@ import org.utnrepasa.net.Connection;
 import org.utnrepasa.net.RequestAction;
 
 /**
+ * Solicita una cantidad n de usuarios a partir del nombre de usuario dado
+ * como un patrón.
  *
  * @author Parisi Germán
  * @version 1.0
  */
-public class SearchUsersRequestAction implements RequestAction{
+public class SearchUsersRequestAction implements RequestAction {
 
     private static final long serialVersionUID = 5;
+    /**
+     * Id del jugador solicitante.
+     */
     private int id;
+    /**
+     * Es la cantidad máxima de usuarios solicitados.
+     */
     private int n;
+    /**
+     * Es el nombre de usuario por el cual buscar. En el servidor se lo
+     * interpreta como un patrón, por ejemplo, el patrón J traerá a todos los
+     * usuarios que tengan una J en su nombre de usuario.
+     */
     private String nombre;
-    public SearchUsersRequestAction(int id, int n, String nombre){
+
+    public SearchUsersRequestAction(int id, int n, String nombre) {
         this.id = id;
         this.n = n;
         this.nombre = nombre;
     }
+
     @Override
     public int getId() {
         return this.id;
--- src/org/utnrepasa/net/response/SearchUsersResponseAction.java
+++ src/org/utnrepasa/net/response/SearchUsersResponseAction.java
@@ -7,17 +7,24 @@ import org.utnrepasa.net.principal.ControladorCliente;
 import org.utnrepasa.net.util.User;
 
 /**
+ * Retorna una lista de usuarios.
  *
  * @author Parisi Germán
  * @version 1.0
  */
-public class SearchUsersResponseAction implements ResponseAction{
+public class SearchUsersResponseAction implements ResponseAction {
 
     private static final long serialVersionUID = 105;
+    /**
+     * Es la lista de usuarios. Puede ser null en caso que los parámetros
+     * sean incorrectos.
+     */
     private ArrayList<User> usuarios;
-    public SearchUsersResponseAction(ArrayList<User> usuarios){
+
+    public SearchUsersResponseAction(ArrayList<User> usuarios) {
         this.usuarios = usuarios;
     }
+
     @Override
     public void execute(Connection connection) {
         ControladorCliente.getInstancia().recibirUsuarios(usuarios);
