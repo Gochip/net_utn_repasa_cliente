8bc7eebae5bd79531094201723eb619c856b7da1
Gochi <germannparisi@gmail.com>
2014-10-12 16:59:59 -0300

    Arreglo de nombrado

---
+++ src/org/utnrepasa/net/response/CreationMultiplayerResponseAction.java
@@ -0,0 +1,41 @@
+package org.utnrepasa.net.response;
+
+import org.utnrepasa.net.Connection;
+import org.utnrepasa.net.ResponseAction;
+import org.utnrepasa.net.principal.ControladorCliente;
+
+/**
+ *
+ * @author Parisi Germán
+ * @version 1.0
+ */
+public class CreationMultiplayerResponseAction implements ResponseAction {
+
+    private static final long serialVersionUID = 103;
+    /**
+     * Este atributo indica si la partida fue creada con éxito.
+     */
+    private final boolean creacionCorrecta;
+    /**
+     * Este atributo no es muy útil pero el servidor lo devuelve por seguridad.
+     * Para saber que el que está recibiendo este objeto es realmente el jugador
+     * correspondiente.
+     */
+    private final int idUsuario;
+
+    public CreationMultiplayerResponseAction(int idUsuario, boolean creacionCorrecta) {
+        this.idUsuario = idUsuario;
+        this.creacionCorrecta = creacionCorrecta;
+    }
+
+    @Override
+    public void execute(Connection connection) {
+        ControladorCliente controlador = ControladorCliente.getInstancia();
+        if (idUsuario == controlador.getUsuario().getId()) {
+            controlador.respuestaCreacionPartida(creacionCorrecta);
+        }else{
+            // Algo pasó... El servidor me tiró un objeto que no me pertenece.
+        }
+        connection.close();
+    }
+}
